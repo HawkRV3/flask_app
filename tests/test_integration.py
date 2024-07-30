a030855c606fd71d7c1604c1d7a3b7463c9cb99f
hawkrv3 <hawkrv3@hawkrv3>
2024-07-30 17:25:36 -0600

    Eliminar todos los archivos del repositorio

--- tests/test_integration.py
+++
@@ -1,7 +0,0 @@
-import pytest 
-import requests
-
-def test_index():
-	response = requests.get('<http://localhost:5000/>')
-	assert response.status_code == 200
-	assert response.json()['message'] == 'Hello, World!'
